ede2582088c71c5c631e5d3cc47d05bc01bd0ec1
Justin Bell <justinbell0114@gmail.com>
2020-08-03 11:56:17 -0400

    Basic Player Scripts

---
+++ TeamGame1/Assets/PlayerScript/MouseAim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAim : MonoBehaviour
+{
+    public Transform objectToMove;
+    Vector3 mouse;
+    Ray castPoint;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = Input.mousePosition;
+        castPoint = Camera.main.ScreenPointToRay(mouse);
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+           
+            objectToMove.transform.position = new Vector3(hit.point.x, 43 ,hit.point.z);
+        }
+    }
+}
---
+++ TeamGame1/Assets/PlayerScript/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100;
+    public bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        takeDamage(10);
+        Debug.Log(health);
+    }
+    public void takeDamage(int Damage)
+    {
+        health = health - Damage;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            isDead = true;
+        }
+    }
+}
