b7b7980070517bc7d6003211df371e3ef1421cdd
SaniyaKalamkar <68800346+SaniyaKalamkar@users.noreply.github.com>
2020-08-04 07:58:20 -0700

    Nav Mesh and Bullet shooting

---
+++ TeamGame1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    NavMeshAgent Enemy;
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.SetDestination (Player.transform.position);
+    }
+}
