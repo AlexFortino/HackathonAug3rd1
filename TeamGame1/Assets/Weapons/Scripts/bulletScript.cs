a38c7a6507b387afdff26dbf7c1832120dcea959
TunaChef <68859305+TunaChef@users.noreply.github.com>
2020-08-04 12:19:24 -0400

    Started Work on Ar and gun hierarchy

--- TeamGame1/Assets/Weapons/Scripts/bulletScript.cs
+++ TeamGame1/Assets/Weapons/Scripts/bulletScript.cs
@@ -8,7 +8,7 @@ public class bulletScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
--- TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
+++ TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class weaponScript : MonoBehaviour
 {
     public GameObject bullet;
+    public float damage = 10f;
+    public float bSpeed = 10f;
+    public float shotDelay = 10f;
+    public float reloadTime = 2f;
+    public int shotCount = 1;
+    public float accuracy = 1f;
+    private bool canShoot = true;
+    private bool fullReload = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +24,20 @@ public class weaponScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Instantiate(bullet, this.transform.position, this.transform.rotation);
+        if (Input.GetButtonDown("Fire1") && canShoot)
+        {
+            canShoot = false;
+            Instantiate(bullet, this.transform.position, this.transform.rotation);
+        }
+        if(Input.GetButton("Fire1") && canShoot)
+        {
+            canShoot = false;
+            Instantiate(bullet, this.transform.position, this.transform.rotation);
+        }
+        canShoot = true;
+    }
+    void fire()
+    {
+
     }
 }
