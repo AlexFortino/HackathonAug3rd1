f3b3ed97ea5a6315037fe42a360fd30796813b60
Justin Bell <justinbell0114@gmail.com>
2020-08-05 02:01:37 -0400

    Some Fixes..

--- TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
+++ TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class weaponScript : MonoBehaviour
 {
     public GameObject bullet;
+    public GameObject gun;
+    public bool shoot = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    void shoots()
+    {
+        Instantiate(bullet, this.transform.position, this.transform.rotation);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(bullet, this.transform.position, this.transform.rotation);
+        if (Input.GetMouseButton(0))
+        {
+            Invoke("shoots", 1);
+        }
+        
     }
 }
