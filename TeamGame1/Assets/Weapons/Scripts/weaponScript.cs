e952fe70092e5c417cb60256629fc15576ecee11
Justin Bell <justinbell0114@gmail.com>
2020-08-05 11:47:30 -0400

    Nothing really...

--- TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
+++ TeamGame1/Assets/Weapons/Scripts/weaponScript.cs
@@ -23,7 +23,7 @@ public class weaponScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
    
     // Update is called once per frame
