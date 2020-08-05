cc591a0a955212caec4cce9e23713d31249b6693
TunaChef <68859305+TunaChef@users.noreply.github.com>
2020-08-05 11:44:16 -0400

    AR and Pistol work

---
+++ TeamGame1/Assets/Weapons/Scripts/ARScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARScript : weaponScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotDelay = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && canShoot)
+        {
+            canShoot = false;
+            Instantiate(bullet, transform.position, transform.rotation);
+            Invoke("shotCooldown", shotDelay);
+        }
+
+    }
+}
