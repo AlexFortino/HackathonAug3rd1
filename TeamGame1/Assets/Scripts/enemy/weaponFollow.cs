ef6af2895d6f2be4e6f10851d26ed5e2380b1a69
SaniyaKalamkar <68800346+SaniyaKalamkar@users.noreply.github.com>
2020-08-06 08:15:52 -0700

    Animations and Enemy switch weapons

---
+++ TeamGame1/Assets/Scripts/enemy/weaponFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponFollow : MonoBehaviour
+{   
+    GameObject Enemy;
+    GameObject Player;
+
+    int currentWeapon = 1;
+    int previousWeapon = 1;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Player = GameObject.FindGameObjectWithTag("Player");
+        currentWeapon = Player.GetComponent<PlayerWeaponInteract>().getPreviousWeapon();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
